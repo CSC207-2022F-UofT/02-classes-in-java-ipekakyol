27b7f058a8b343cedeecdafba545d9467db9b45b
ipekakyol <66427281+ipekakyol@users.noreply.github.com>
2022-10-06 18:00:04 -0400

    Modifying Task 1

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -84,7 +84,6 @@ public class Silly implements Comparable<Silly>{
     }
 
 
-
     public static void main(String[] args) {
         /**
          * Task 1 (continued): The below demonstrates how each of the Silly
